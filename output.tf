8be5d37b0089aacab035e6de9d9286b902f1c559
ramak8sgcp <ramak8sgcp@gmail.com>
2025-02-04 10:30:20 +0530

    terraform-aws-vpc

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
-    value = module.vpc.vpc_id 
+  value = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-    value = module.vpc.public_subnets_ids
+  value = module.vpc.public_subnets_ids
 }
 
 # output "az_info" {
